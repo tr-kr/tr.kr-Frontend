acb4fc2e79f4f30a99ee07eaf1b087a9d76a223d
bishoe01 <bishoe01@gmail.com>
2023-07-24 17:17:57 +0900

    #add font적용 / navbar

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [navItems, setNavItems] = useState<string[]>([
+    '서비스 소개 ',
+    '대회 탐색',
+    '대회 생성',
+  ]);
+  return (
+    <header
+      className="
+    flex items-center
+    justify-between
+    px-6
+    py-4
+    bg-black
+    text-white
+    "
+      text-white
+    >
+      <h1 className="text-2xl font-bold">TR.KR</h1>
+      <ul className="flex items-center gap-4">
+        {navItems.map((item) => (
+          <li className="" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
