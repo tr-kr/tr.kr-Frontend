47adf408a6457b01f59214760297e58b7da96a28
bishoe01 <bishoe01@gmail.com>
2023-07-24 16:07:56 +0900

    ADD : tailwindcss적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
 });
