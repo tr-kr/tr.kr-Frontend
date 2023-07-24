91902dede3ecf686491fdef81f2a9d6579410d07
kimcurly <sanbonai0728@gmail.com>
2023-07-24 16:45:20 +0900

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/UI/Header';
 
 function App() {
-  return <div className="text-2xl">gi</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import Home from './pages/Home.tsx';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error/>,
+    children: [{ index: true, path: '/', element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
