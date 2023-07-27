fa885529130bf8ae753c878ba64a3f10c6230db7
kimcurly <sanbonai0728@gmail.com>
2023-07-27 18:22:25 +0900

    feat: 대회 배너 이미지 업로드 버튼 구현

--- src/pages/CompCreatePage.tsx
+++ src/pages/CompCreatePage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import CompState from '../components/CreateComp/CompState';
+import CompImageUpload from '../components/CreateComp/CompImageUpload';
 
 const CompCreatePage = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <CompState />
+      <CompImageUpload />
     </div>
   );
 };
