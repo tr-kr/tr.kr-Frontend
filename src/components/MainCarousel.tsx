5764ba85854e44978f4e513ec75967083e3e98e7
bishoe01 <bishoe01@gmail.com>
2023-07-27 20:39:37 +0900

    [feat] carousel 개선

--- src/components/MainCarousel.tsx
+++ src/components/MainCarousel.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const MainCarousel = () => {
     return (
-        <Carousel
-            autoPlay
-            infiniteLoop
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            interval={5000}
-            showArrows={true}
-        >
-            <div>
-                <img src="https://images.unsplash.com/photo-1558981806-ec527fa84c39" alt="Image 1" />
-                <p className="legend">Legend 1</p>
-            </div>
-            <div>
-                <img src="https://images.unsplash.com/photo-1677611998429-1baa4371456b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2664&q=80" alt="Image 2" />
-                <p className="legend">Legend 2</p>
-            </div>
-            <div>
-                <img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691" alt="Image 3" />
-                <p className="legend">Legend 3</p>
-            </div>
-        </Carousel>
+        <div className="rounded-xl border-2 border-gray-300 overflow-hidden w-4/5 mx-auto"> 
+            <Carousel
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                interval={5000}
+                showArrows={true}
+            >
+                <div>
+                    <img src="https://images.unsplash.com/photo-1558981806-ec527fa84c39" alt="Image 1" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1677611998429-1baa4371456b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2664&q=80" alt="Image 2" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691" alt="Image 3" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        </div>
     );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import MainCarousel from '../components/MainCarousel';
 
 const Home = () => {
+  const category: string[] = ['인기순', '참여자순', '게임인기순'];
     return (
-        <div>
+        <section>
+          <div className='flex justify-between items-center px-8 py-4'>
+            <h1 className='font-B text-4xl'>주목해야할 TR.KR 대회</h1>
+          </div>
             <MainCarousel />
             {/* 다른 컴포넌트들 */}
-        </div>
+        </section>
     );
 };
 
