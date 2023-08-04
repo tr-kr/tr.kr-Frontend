8330108c164b09ec244497d695a30c268c7cef14
typeYu <105202875+typeYu@users.noreply.github.com>
2023-08-04 13:10:55 +0900

    save

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import banner from '../../image/banner.png'
 
 const Result = () => {
 
     const backendData = ['xxxxx', 'xxxx', 'xxxxxxxxxxx', 'xxxx', 'xxxxxxxx', 'xxxxx'];
 
     return (
-        <div className="h-screen mt-[30px] ml-[10%] mr-[10%]">
-            <div className="flex justify-between">
+        <div className="h-screen mb-[10%]">
+            {/* {배너 관련 코드} */}
+            <div>
+                <div className="w-full h-[504px] bg-cover bg-center relative">
+                    {/* 배경 이미지 설정 */}
+                    <img src={banner} alt="배너" className="w-full h-full" />
+
+                    {/* 맨 아래쪽에 겹치는 박스 */}
+                    <div className="flex items-center absolute bottom-0 left-0 w-full h-[139px] bg-black bg-opacity-50 text-[64px] text-white font-bold pl-10">VX OPEN CHALLENGE</div>
+                </div>
+            </div>
+            {/* {배너 관련 코드} */}
+            <div className="flex justify-between mt-[30px] ml-[10%] mr-[10%]">
                 <div className="text-white text-[40px]">VX OPEN CHALLENGE</div>
                 <div className="flex items-center">
                     <button className="w-[216.16px] h-[34.53px] text-white text-[20px] ml-auto rounded-[25px] border-[3px] border-white mr-[10px]">
@@ -17,7 +29,7 @@ const Result = () => {
                     </button>
                 </div>
             </div>
-            <div className="flex mt-[10px]">
+            <div className="flex mt-[10px] ml-[10%] mr-[10%]">
                 <div className="w-[104.68px] h-[34.53px] bg-[#3f4fe1] text-white text-[20px] text-center rounded-[25px] border-[3px] border-white">
                     진행중
                 </div>
--- src/components/RuleBook/RuleBook.tsx
+++ src/components/RuleBook/RuleBook.tsx
@@ -1,18 +1,32 @@
+import banner from '../../image/banner.png'
 
 const RuleBook = () => {
     return (
-        <div className="h-screen mt-[60px] ml-[10%] mr-[10%]">
+        <div className="h-screen">
+            {/* {배너 관련 코드} */}
+            <div>
+                <div className="w-full h-[504px] bg-cover bg-center relative">
+                    {/* 배경 이미지 설정 */}
+                    <img src={banner} alt="배너" className="w-full h-full" />
+
+                    {/* 맨 아래쪽에 겹치는 박스 */}
+                    <div className="flex items-center absolute bottom-0 left-0 w-full h-[139px] bg-black bg-opacity-50 text-[64px] text-white font-bold pl-10">VX OPEN CHALLENGE</div>
+                </div>
+            </div>
+            {/* {배너 관련 코드} */}
             {/* 버튼 스타일을 적용한 예시 버튼 */}
-            <button className="w-[125px] h-[51px] bg-[#3f4fe1] text-white text-[32px]">
-                규정집
-            </button>
+            <div className="mt-[60px] ml-[10%] mr-[10%]">
+                <button className="w-[125px] h-[51px] bg-[#3f4fe1] text-white text-[32px]">
+                    규정집
+                </button>
                 <div className="border-t-[3px] border-gray-400 mt-[10px] mb-[10px] "></div>
 
                 <div className="w-full h-2/3 bg-[grey] overflow-auto">
-                    <img src="https://www.gnmedia.or.kr/data/img/[%ED%8F%AC%EC%8A%A4%ED%84%B0]%202022%20%EA%B8%B0%EC%97%AD%EB%8B%88%EC%9D%80_fin.png"></img>
+                    <img src="https://www.jj.ac.kr/_attach/_file//editor_viewer/out/_attach/_file/jj/2018/07/7d62d9328ad0b46ebe71589dbea13b4c.pdf.files/00002.png" className="w-full h-full object-cover"></img>
                 </div>
 
                 <div className="border-t-[3px] border-gray-400 mt-[10px] mb-[10px] "></div>
+            </div>
         </div>
     );
 };
