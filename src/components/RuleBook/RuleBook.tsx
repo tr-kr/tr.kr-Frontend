503ac732ed8a3859bb035228b52bf3efecdd15d7
typeYu <105202875+typeYu@users.noreply.github.com>
2023-08-20 13:02:12 +0900

    백엔드에서 odf 가져오기 완료

--- src/components/RuleBook/RuleBook.tsx
+++ src/components/RuleBook/RuleBook.tsx
@@ -3,15 +3,15 @@ import axios from 'axios';
 
 const RuleBook = () => {
     const [pdfPath, setPdfPath] = useState<string | null>(null);
+    const fixedBaseUrl = "http://3.34.170.230";
 
     useEffect(() => {
-        axios.get("http://3.34.170.230/api/competition/1")
+        axios.get("http://3.34.170.230/api/competition/2")
             .then(response => {
                 const responseData = response.data;
                 if (responseData.result && responseData.result.length > 0) {
                     const pdfPathFromAPI = responseData.result[0].pdf_path;
                     const modifiedPdfPath = pdfPathFromAPI.replace('/public', '');
-                    console.log('PDF Path:', modifiedPdfPath); // pdfPath 값 출력
                     setPdfPath(modifiedPdfPath);
                 }
             })
@@ -22,7 +22,6 @@ const RuleBook = () => {
 
     return (
         <div className="h-screen mt-[60px] ml-[17%] mr-[17%]">
-            {/* 버튼 스타일을 적용한 예시 버튼 */}
             <button className="w-[125px] h-[51px] bg-[#3f4fe1] text-white text-[32px]">
                 규정집
             </button>
@@ -30,7 +29,7 @@ const RuleBook = () => {
 
             <div className="w-full h-3/4 bg-[grey] overflow-auto">
                 {pdfPath && (
-                    <iframe className="w-full h-full bg-[grey] overflow-auto" src={pdfPath}></iframe>
+                    <iframe className="w-full h-full bg-[grey] overflow-auto" src={`${fixedBaseUrl}/${pdfPath}`}></iframe>
                 )}
             </div>
 
