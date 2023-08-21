efba794ca93817fb803d6a18d234e6ba3b834aec
kimcurly <sanbonai0728@gmail.com>
2023-08-22 02:16:29 +0900

    feat: 대진표 준결승전 UI 추가

---
+++ src/components/Bracket/SemiFinal.tsx
@@ -0,0 +1,11 @@
+const SemiFinal = () => {
+  return (
+    <div className="flex flex-col justify-between items-center relative h-[150px]">
+      <div className="w-[145px] h-[42px] rounded-[7px] border-[1px] border-solid border-[#fff] bg-[#d9d9d94d]"></div>
+      <div className="w-[145px] h-[42px] rounded-[7px] border-[1px] border-solid border-[#fff] bg-[#d9d9d94d]"></div>
+      <div className="absolute top-[15px] -right-[80px] w-[80px] h-[120px] border-l-[0px] border-r border-y border-solid border-[#fff]"></div>
+    </div>
+  );
+};
+
+export default SemiFinal;
