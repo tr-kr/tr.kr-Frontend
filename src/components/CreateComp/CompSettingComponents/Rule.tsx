a6d130bb9cfefe389bc52e593337e93b7d68ec13
kimcurly <sanbonai0728@gmail.com>
2023-08-22 18:48:19 +0900

    feat: 규정집 pdf 파일 업로드 시 파일 이름 미리보기 기능 추가

--- src/components/CreateComp/CompSettingComponents/Rule.tsx
+++ src/components/CreateComp/CompSettingComponents/Rule.tsx
@@ -1,19 +1,41 @@
+import { useState } from 'react';
 import Identifier from '../../UI/Identifier';
 
 const Rule = () => {
+  const [pdfFile, setPdfFile]: any = useState();
+
+  const saveUploadedPdfFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const fileName = e.target.files[0].name;
+      // const parsedFileName = fileName.replace(/\s/g, '_');
+      // setPdfFile(`before:content-['${parsedFileName}']`);
+      setPdfFile(fileName);
+    }
+  };
+
+  console.log(pdfFile);
   return (
     <div className="flex justify-start items-start w-[40.125rem] mt-[85px]">
       <div className="h-[45px] relative">
         <Identifier title="규정" color="primary" />
       </div>
-      <form className="flex flex-col pt-[5px] ml-[25px]">
-        <div className="w-[240px] h-auto mb-1">
-          <label htmlFor="pdf">
-            <div className="flex justify-center items-center w-[134px] h-[37px] rounded-[7px] border-2 border-solid border-white">
+      <form className="flex flex-col pt-[5px] w-full ml-[25px]">
+        <div className="mb-1">
+          <label htmlFor="pdf" className="flex items-center w-full">
+            <div className="flex justify-center items-center min-w-[134px] h-[37px] rounded-[7px] border-2 border-solid border-white">
               <div className="text-white font-R text-[15px]">
                 + PDF 추가하기
-                <input className="hidden" type="file" id="pdf" accept=".pdf" />
+                <input
+                  className="hidden"
+                  type="file"
+                  id="pdf"
+                  accept=".pdf"
+                  onChange={saveUploadedPdfFile}
+                />
               </div>
+            </div>
+            <div className="flex justify-start items-center w-full ml-5 text-[16px] text-[#00ff47] font-R">
+              {pdfFile && pdfFile}
             </div>
           </label>
         </div>
