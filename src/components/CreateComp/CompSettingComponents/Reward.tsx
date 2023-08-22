42c224ba752a7d0d1e1bacbc725e1f383c0b2762
kimcurly <sanbonai0728@gmail.com>
2023-08-22 16:33:37 +0900

    fix: 보상 입력 컴포넌트 3, 4위전 삭제에 따른 보상 1, 2위 두 개로 축소

--- src/components/CreateComp/CompSettingComponents/Reward.tsx
+++ src/components/CreateComp/CompSettingComponents/Reward.tsx
@@ -3,7 +3,7 @@ import Identifier from '../../UI/Identifier';
 const Reward = () => {
   return (
     <div className="flex justify-start items-start w-[40.125rem] mt-[85px]">
-      <div className="h-[340px] relative">
+      <div className="h-[227px] relative">
         <Identifier title="보상" color="primary" />
       </div>
       <form className="flex flex-col ml-[25px]">
@@ -28,17 +28,7 @@ const Reward = () => {
               />
             </div>
           </div>
-          <div className="mb-[42px]">
-            <div className="font-L text-white text-[20px] mb-[7px]">3위</div>
-            <div className="flex justify-between items-center w-full">
-              <input
-                type="text"
-                placeholder="보상을 입력하세요"
-                className="w-full h-[36px] pl-[16px] text-[#9F9F9F] rounded border border-solid border-[#fff] bg-[#d9d9d91a]"
-              />
-            </div>
-          </div>
-          <div className="flex justify-center items-center w-[134px] h-[37px] rounded-[7px] border-2 border-solid border-white">
+          <div className="flex justify-center items-center w-[134px] h-[37px] rounded-[7px] border-2 border-solid border-white cursor-pointer">
             <div className="text-white font-R text-[15px]">+ 보상 추가하기</div>
           </div>
         </div>
