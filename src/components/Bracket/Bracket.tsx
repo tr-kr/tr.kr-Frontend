8037b434a74595b67d80ca842b8c2c5cec4fccc8
kimcurly <sanbonai0728@gmail.com>
2023-08-22 13:34:02 +0900

    feat: 백엔드 데이터 연동 기본 세팅

--- src/components/Bracket/Bracket.tsx
+++ src/components/Bracket/Bracket.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from 'react';
 import Final from './Final';
 import Qualifer from './Qualifer';
 import SemiFinal from './SemiFinal';
+import axios from 'axios';
 
 const Bracket = () => {
+  const [bracket, setBracket] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(
+        'http://3.34.170.230/api/competition/entry/1',
+      );
+      console.log(data.data);
+    };
+    fetchData();
+  });
+
   return (
     <div className="flex justify-center items-center w-[900px] h-[500px] bg-[#101415] border-[1px] border-solid border-[#374c52]">
       <div className="flex justify-between items-center w-[610px]">
