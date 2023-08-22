2d92d11622b17666d59e2ce42a7cf173c7e00880
typeYu <105202875+typeYu@users.noreply.github.com>
2023-08-22 14:09:16 +0900

    save

--- src/components/CompOutline/CompOutline.tsx
+++ src/components/CompOutline/CompOutline.tsx
@@ -2,9 +2,18 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import moment from "moment";
 
 const CompOutline = () => {
     const backendInfo = ['...'];
+    const marks = [
+        "15-08-2023",
+        "16-08-2023",
+        "17-08-2023",
+        "18-08-2023",
+        "19-08-2023",
+        "20-08-2023",
+      ];
     const [isCalendarVisible, setIsCalendarVisible] = useState(false);
     const [competitionInfo, setCompetitionInfo] = useState<{
         id: number;
@@ -89,6 +98,11 @@ const CompOutline = () => {
                                 date.toLocaleDateString('en', { weekday: 'short' }).toUpperCase().slice(0, 1)
                             }
                             locale="en-US"
+                            tileClassName={({ date, view }) => {
+                                if (marks.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
+                                  return "highlight";
+                                }
+                              }}
                         />
                     </div>
                 </div>
