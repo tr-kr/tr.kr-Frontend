6899017fbbc9ede36f4de8308211a0c0b466a142
bishoe01 <bishoe01@gmail.com>
2023-08-24 03:45:07 +0900

    error 수정

--- src/main.tsx
+++ src/main.tsx
@@ -10,17 +10,16 @@ import League from './pages/League.tsx';
 import MyPage from './components/MyPage/MyPage.tsx';
 import CompCreatePage from './pages/CompCreatePage';
 import MyLeague from './pages/MyLeague.tsx';
-import RuleBook from './components/RuleBook/RuleBook.tsx'
-import ParticipatingTeam from './components/ParticipatingTeam/ParticipatingTeam.tsx'
-import CompOutline from './components/CompOutline/CompOutline.tsx'
-import Result from './components/Result/Result.tsx'
-import './index.css'
+import RuleBook from './components/RuleBook/RuleBook.tsx';
+import ParticipatingTeam from './components/ParticipatingTeam/ParticipatingTeam.tsx';
+import CompOutline from './components/CompOutline/CompOutline.tsx';
+import Result from './components/Result/Result.tsx';
+import './index.css';
 import Login from './components/Login/Login.tsx';
 import Signup from './components/Login/Signup.tsx';
 import BracketPage from './pages/BracketPage.tsx';
 import ParticipatePage from './pages/ParticipatePage.tsx';
 import Banner from './components/Banner/Banner.tsx';
-
 
 const router = createBrowserRouter([
   {
@@ -50,17 +49,18 @@ const router = createBrowserRouter([
       {
         path: '/mypage/league/:id',
         element: <MyLeague />,
-
-        path: 'login', 
-        element: <Login />
       },
       {
-        path: 'signup', 
-        element: <Signup />
+        path: 'login',
+        element: <Login />,
       },
       {
-        path: 'result', 
-        element: <Result />
+        path: 'signup',
+        element: <Signup />,
+      },
+      {
+        path: 'result',
+        element: <Result />,
       },
       {
         path: 'banner',
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,6 @@ import MainCarousel from '../components/MainCarousel';
 const Home = () => {
   return (
     <div className="bg-gray pt-2">
-      {/* <div className="flex justify-between items-center px-8 py-4">
-          <h1 className="font-EB text-4xl text-white">TR.KR 대회 리스트</h1>
-        </div> */}
       <MainCarousel />
     </div>
   );
