1befe97e8c0c8a4c1ec0327f3e2d736bc2b2d31f
kimcurly <sanbonai0728@gmail.com>
2023-08-24 15:14:59 +0900

    feat: 대회 정보 입력 기능 추가

--- src/components/CreateComp/CompSettingComponents/Information.tsx
+++ src/components/CreateComp/CompSettingComponents/Information.tsx
@@ -1,6 +1,14 @@
 import Identifier from '../../UI/Identifier';
+import { CompSettingProps } from '../CompSetting';
 
-const Information = () => {
+const Information = ({ compSetting, setCompSetting }: CompSettingProps) => {
+  const handleCompName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompSetting({ ...compSetting, competition_title: e.target.value });
+  };
+
+  const handleCompIntroduce = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCompSetting({ ...compSetting, competition_content: e.target.value });
+  };
   return (
     <div className="flex justify-start items-start w-[40.125rem] mt-[71px]">
       <div className="h-[122px] relative">
@@ -11,12 +19,14 @@ const Information = () => {
           <input
             type="text"
             placeholder="대회명"
+            onChange={handleCompName}
             className="w-[536px] h-[32px] pl-[13px] text-white rounded-[7px] border border-solid border-white bg-[#d9d9d91a] placeholder:font-R placeholder:text-[#9F9F9F] placeholder:text-[14px]"
           />
         </div>
         <div>
           <textarea
             placeholder="대회 한 줄 소개"
+            onChange={handleCompIntroduce}
             className="w-[536px] h-[82px] pl-[13px] pt-[7px] resize-none text-white rounded-[7px] border border-solid border-white bg-[#d9d9d91a] placeholder:font-R placeholder:text-[#9F9F9F] placeholder:text-[14px]"
           />
         </div>
