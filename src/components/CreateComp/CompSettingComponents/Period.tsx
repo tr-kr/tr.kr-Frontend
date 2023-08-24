6fc4a9f65909e937460cce4724617c578c92010b
kimcurly <sanbonai0728@gmail.com>
2023-08-24 15:15:51 +0900

    feat: 대회 기간 입력 기능 추가

--- src/components/CreateComp/CompSettingComponents/Period.tsx
+++ src/components/CreateComp/CompSettingComponents/Period.tsx
@@ -1,6 +1,49 @@
+import { useEffect, useState } from 'react';
 import Identifier from '../../UI/Identifier';
+import { CompSettingProps } from '../CompSetting';
 
-const Period = () => {
+const Period = ({ compSetting, setCompSetting }: CompSettingProps) => {
+  const [compRecruitPeriod, setCompRecruitPeriod] = useState('');
+  const [compRecruitSecondPerid, setCompRecruitSecondPerid] = useState('');
+  const [compPeriod, setCompPeriod] = useState('');
+  const [compSecondPeriod, setCompSecondPeriod] = useState('');
+
+  const handleCompRecruit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // if (compRecruitPeriod) setCompRecruitPeriod('');
+    setCompRecruitPeriod(e.target.value);
+  };
+
+  const handleCompSecondRecruit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // if (compRecruitPeriod) setCompRecruitPeriod('');
+    setCompRecruitSecondPerid(e.target.value);
+  };
+  const handleCompPeriod = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // if (compPeriod) setCompPeriod('');
+    setCompPeriod(e.target.value);
+  };
+
+  const handleCompSecondPeriod = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // if (compPeriod) setCompPeriod('');
+    setCompSecondPeriod(e.target.value);
+  };
+
+  useEffect(() => {
+    if (
+      compRecruitPeriod &&
+      compRecruitSecondPerid &&
+      compPeriod &&
+      compSecondPeriod
+    ) {
+      const recruitPeriod = compRecruitPeriod + ',' + compRecruitSecondPerid;
+      const period = compPeriod + ',' + compSecondPeriod;
+      setCompSetting({
+        ...compSetting,
+        recruit_period: recruitPeriod,
+        competition_period: period,
+      });
+    }
+  }, [compPeriod, compRecruitPeriod, compSecondPeriod, compRecruitSecondPerid]);
+
   return (
     <div className="flex justify-start items-start w-[40.125rem] mt-[85px]">
       <div className="h-[160px] relative">
@@ -15,11 +58,13 @@ const Period = () => {
             <div className="flex justify-between items-center w-full">
               <input
                 type="date"
+                onChange={handleCompRecruit}
                 className="w-[246px] h-[36px] pl-[16px] pr-[16px] font-R text-[#9F9F9F] rounded border border-solid border-[#fff] bg-[#d9d9d91a]"
               />
               <div className="font-R text-white text-[24px]">~</div>
               <input
                 type="date"
+                onChange={handleCompSecondRecruit}
                 className="w-[246px] h-[36px] pl-[16px] pr-[16px] font-R text-[#9F9F9F] rounded border border-solid border-[#fff] bg-[#d9d9d91a]"
               />
             </div>
@@ -31,11 +76,13 @@ const Period = () => {
             <div className="flex justify-between items-center w-full">
               <input
                 type="date"
+                onChange={handleCompPeriod}
                 className="w-[246px] h-[36px] pl-[16px] pr-[16px] font-R text-[#9F9F9F] rounded border border-solid border-[#fff] bg-[#d9d9d91a]"
               />
               <div className="font-R text-white text-[24px]">~</div>
               <input
                 type="date"
+                onChange={handleCompSecondPeriod}
                 className="w-[246px] h-[36px] pl-[16px] pr-[16px] font-R text-[#9F9F9F] rounded border border-solid border-[#fff] bg-[#d9d9d91a]"
               />
             </div>
